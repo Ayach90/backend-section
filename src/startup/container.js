5c18ed5deaeb5ce114ba487fa47f14bd66f1527a
Àngel Ayach <angel@ayach.es>
2021-01-19 15:52:58 +0100

    controllers

--- src/startup/container.js
+++ src/startup/container.js
@@ -8,7 +8,7 @@ const app = require('.')
 const { HomeService, UserService, IdeaService, CommentService } = require('../services');
 
 //Controllers
-const { HomeController } = require('../controllers');
+const { HomeController, UserController, IdeaController, CommentController } = require('../controllers');
 
 //Routes
 const { HomeRoutes } = require('../routes/index.routes');
@@ -35,7 +35,10 @@ container
         IdeaService: asClass(IdeaService).singleton(),
         CommentService: asClass(CommentService).singleton(),
     }).register({
-        HomeController: asClass(HomeController.bind(HomeController)).singleton()
+        HomeController: asClass(HomeController.bind(HomeController)).singleton(),
+        UserController: asClass(HomeController.bind(UserController)).singleton(),
+        IdeaController: asClass(HomeController.bind(IdeaController)).singleton(),
+        CommentController: asClass(HomeController.bind(CommentController)).singleton()
     }).register({
         HomeRoutes: asFunction(HomeRoutes).singleton()
     }).register({
